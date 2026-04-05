a19c68062a2e41eacfa2eb941eda0fab4ac3ec58
Lexovisaurus451 <lexovisaurus451@gmail.com>
2026-04-05 14:45:44 +0200

    Added a name field. It doesn't work rn...

--- app/src/main/java/com/lexovisaurus/archery/SettingsActivity.kt
+++ app/src/main/java/com/lexovisaurus/archery/SettingsActivity.kt
@@ -11,20 +11,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,8 +67,19 @@ fun Greeting2(modifier: Modifier = Modifier) {
 
         }
         Card(Modifier.padding(16.dp).fillMaxWidth()) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+            }
             Text(stringResource(R.string.settings_card_title), Modifier.padding(16.dp).align(Alignment.CenterHorizontally), textAlign = TextAlign.Center)
-            Text(stringResource(R.string.wip), Modifier.padding(64.dp).align(Alignment.CenterHorizontally), textAlign = TextAlign.Center)
+            var name by remember { mutableStateOf("Android") }
+            OutlinedTextField(
+                value = name,
+                onValueChange = {name = it},
+                label = {
+                    Text("name")
+                },
+                modifier = Modifier.padding(16.dp).align(Alignment.CenterHorizontally)
+            )
         }
     }
 
